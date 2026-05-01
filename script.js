9a93ffded5f1abaf435edde72729e9bea0b23aea
solcee <shaun.patrick.berces@gmail.com>
2026-05-01 22:01:37 +0800

    Feat: NavBar Rework

---
+++ script.js
@@ -0,0 +1,19 @@
+let navbar = document.getElementById("navbar");
+navbar.setAttribute("class", "bordered");
+let links = {
+    "about": "about.html",
+    "portfolio": "portfolio.html",
+    "blog": "blog.html",
+    "contact": "contact.html"
+}
+
+
+for (let link in links) {
+  let navbaritem = document.createElement("li");
+  let a = document.createElement("a");
+  a.href = links[link];
+  a.textContent = link;
+  navbaritem.appendChild(a);
+  navbar.appendChild(navbaritem);
+}
+
